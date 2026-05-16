231b2b71af5d0427cd220b4fb33615e7d7ac272c
isabelalopes67 <costaisabelalopes@gmail.com>
2026-05-16 09:49:23 -0300

    adicionando teste

---
+++ api.py
@@ -0,0 +1,15 @@
+#Instalar bibliotecas 
+#pip install requests
+
+#Segundo passo: adicionar/importar ao código 
+import requests 
+
+cep = input("Qual seu cep?")
+
+url = f"https://viacep.com.br/ws/{cep}/json/"
+
+dados1 = requests.get(url)
+
+dadosfinal = dados1.json()
+
+print(dadosfinal)
