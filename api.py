3f19d5566fff62acc2489c6d711079572a6a0b1f
isabelalopes67 <costaisabelalopes@gmail.com>
2026-05-16 11:38:26 -0300

    ignofando info pesspal

--- api.py
+++ api.py
@@ -4,12 +4,27 @@
 #Segundo passo: adicionar/importar ao código 
 import requests 
 
+nome = input("digite o seu nome: ")
+email = input("digite o seu e-mail: ")
+telefone = input("digite seu telefone: ")
+#colhe o cep
 cep = input("Qual seu cep?")
 
+#cria uma vaeiavel e atribuiu o resultado do link
+#utilizamos o f string, para conseguir inserir uma variavel 
 url = f"https://viacep.com.br/ws/{cep}/json/"
 
-dados1 = requests.get(url)
+dados = requests.get(url).json()
 
-dadosfinal = dados1.json()
+print(f"Bem vindo ao Mercado Livre {nome}! o seu e-mail é {email}. O seu telefone é {telefone}. Você mora na rua {dados['logradouro']}, na cidade {dados['localidade']}, no estado de {dados['estado']}.") 
 
-print(dadosfinal)
+#atribuindo variaveis para cada um dos resultados
+# rua = dados['logradouro']
+# bairro = dados['bairro']
+# cidade = dados['localidade']
+
+
+# print(rua)
+# print(bairro)
+# print(cidade)
+
